f76c3f22c17f690b776ad781cb2242b4ebc4b271
VD <valicsekdavid@VDs-MacBook-Pro.local>
2019-12-01 15:33:32 +0100

    Redirect the user when he/she logged out

--- src/main/java/com/example/sportsbetting/config/WebSecurityConfig.java
+++ src/main/java/com/example/sportsbetting/config/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
                 .permitAll();
     }
 
