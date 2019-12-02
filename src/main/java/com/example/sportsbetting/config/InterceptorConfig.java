8796104876fd540edfca26224e7bb385a4003793
David Valicsek <entriek@gmail.com>
2019-12-02 23:02:00 +0100

    Enable the usage of multiple languages using lang param

--- src/main/java/com/example/sportsbetting/config/InterceptorConfig.java
+++ src/main/java/com/example/sportsbetting/config/InterceptorConfig.java
@@ -2,9 +2,17 @@ package com.example.sportsbetting.config;
 
 import com.example.sportsbetting.interceptor.AuthInterceptor;
 import com.example.sportsbetting.interceptor.LanguageInterceptor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
@@ -15,5 +23,28 @@ public class InterceptorConfig implements WebMvcConfigurer {
         // It manage the languages based on query.
         registry.addInterceptor(new LanguageInterceptor());
     }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:locale/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("en", "US"));
+        return localeResolver;
+    }
+
 
 }
